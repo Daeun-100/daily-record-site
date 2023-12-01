8fdc44dcc0fabb9b828f9ec1a0c7f4d3a429252c
Daeun-100 <drechsu@gmail.com>
2023-12-01 21:29:52 +0900

    수정 기능 오류 고침

--- dist/submit.js
+++ dist/submit.js
@@ -43,7 +43,8 @@ export class ClickSubmit extends BaseComponent {
             let date = new Date();
             let year = date.getFullYear();
             let month = date.getMonth() + 1;
-            let todate = date.getDate();
+            let todateE = date.getDate();
+            let todate = todateE < 10 ? "0" + todateE : todateE;
             objStore.delete(`${year}` + `${month}` + `${todate}`);
         };
     }
